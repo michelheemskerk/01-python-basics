a6350e5856a67b036d5e2f1457a08ea101defeb4
Michel Heemskerk <michel@mikacuracao.com>
2026-07-10 12:18:45 -0400

    Changed my first python program

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ print("Welkom bij de AI Engineering Academy!")
 naam = input("Wat is je naam? ")
 
 print(f"Leuk je te ontmoeten, {naam}!")
-print("Vandaag begint jouw reis als AI Engineer.")
+print("Vandaag begint mijn reis als AI Engineer.")
