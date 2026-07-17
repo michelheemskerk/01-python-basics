5c2590dd908155d69767a66808dc6bae319441b0
Michel Heemskerk <michel@mikacuracao.com>
2026-07-17 14:00:50 -0400

    Added variables and first script to calculate next age

---
+++ calculateAge.py
@@ -0,0 +1,11 @@
+print ("We gaan samen je aanstaande leeftijd berekenen.")
+
+naam = input("Wat is je naam? ")
+huidige_leeftijd = int(input("Wat is je huidige leeftijd?"))
+
+volgende_leeftijd = huidige_leeftijd + 1
+
+message = f"Hallo {naam}, volgend jaar ben je {volgende_leeftijd}"
+
+print (message)
+
---
+++ variables.py
@@ -0,0 +1,30 @@
+name = "Michel"
+age = 52
+temperature = 31.5
+is_learning = True
+
+print(name)
+print(age)
+print(temperature)
+print(is_learning)
+
+print(type(name))
+print(type(age))
+print(type(temperature))
+print(type(is_learning))
+
+birth_year = 1974
+current_year = 2026
+
+age = current_year - birth_year
+
+print(age)
+
+trees = 30
+area = 500
+
+space_per_tree = area / trees
+
+print(space_per_tree)
+print(f"{space_per_tree:.2f}")
+print(type(space_per_tree))
